2bc1f2e4f386796ee2c8a47a3289fe8ba3f58019
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-29 11:45:10 +0100

    feat(ui): Implement loading state for TrendingCoinsList

--- src/components/trending-coins-list.tsx
+++ src/components/trending-coins-list.tsx
@@ -2,13 +2,10 @@
 import useCryptoTrending from "@/hooks/use-fetch-crypto-trending";
 import React from "react";
 import { TrendingCard } from "./cards/trending-card";
+import TrendingCardSkeleton from "./skeleton/trending-car-skeleton";
 
 const TrendingCoinsList: React.FC = () => {
   const { data, error, isLoading } = useCryptoTrending();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   if (error) {
     return <div>{error}</div>;
@@ -17,7 +14,13 @@ const TrendingCoinsList: React.FC = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 max-lg:grid-rows-1 gap-1.5">
       {data?.coins.map((coin) => (
-        <TrendingCard key={coin.item.id} coin={coin.item} />
+        <>
+          {isLoading ? (
+            <TrendingCardSkeleton />
+          ) : (
+            <TrendingCard key={coin.item.id} coin={coin.item} />
+          )}
+        </>
       ))}
     </div>
   );
