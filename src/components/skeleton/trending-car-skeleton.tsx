a39170087cac7f9bfd228e366ffefb2f705d1237
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-29 11:28:55 +0100

    feat(ui): Implement TrendingCardSkeleton component for providing a loading indicator while fetching trending coins.

---
+++ src/components/skeleton/trending-car-skeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const TrendingCardSkeleton = () => {
+  return <Skeleton className="h-24 w-full" />;
+};
+
+export default TrendingCardSkeleton;
