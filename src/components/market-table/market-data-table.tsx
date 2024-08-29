c3780e9a6577944b0bfa20a55ffe7926ea50dd4f
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-29 13:31:41 +0100

    feat(data): Implement data fetching and handle loading/error states

--- src/components/market-table/market-data-table.tsx
+++ src/components/market-table/market-data-table.tsx
@@ -1,7 +1,10 @@
 import { ColumnDef } from "@tanstack/react-table";
 
+import { Ellipsis } from "@/assets/icons";
 import { Table } from "@/components/ui/table";
+import useCryptoMarketData from "@/hooks/use-fetch-crypto-market";
 import { useTableState } from "@/hooks/use-market-table-state";
+import { Button } from "../ui/button";
 import TableBodyComponent from "./market-table-body";
 import TableHeaderComponent from "./market-table-header";
 import Pagination from "./market-table-pagination";
@@ -17,6 +20,7 @@ export function MarketDataTable<TData, TValue>({
   data,
 }: DataTableProps<TData, TValue>) {
   const { table } = useTableState({ columns, data });
+  const { isLoading, error } = useCryptoMarketData();
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     table.getColumn("crypto_info")?.setFilterValue(event.target.value);
@@ -36,11 +40,22 @@ export function MarketDataTable<TData, TValue>({
       <div className="mt-8 border border-tokena-gray dark:border-tokena-dark-gray rounded-xxl">
         <div className="flex justify-between items-center p-4">
           <h3 className="text-tokena-dark text-base font-semibold">Market</h3>
-          <p>Market data provided by CoinGecko API</p>
+          <Button
+            size="icon-xs"
+            variant="icon-button"
+            className="dark:border-tokena-dark-gray dark:text-tokena-light-gray"
+          >
+            <Ellipsis />
+          </Button>
         </div>
         <Table className="min-w-max">
           <TableHeaderComponent table={table} />
-          <TableBodyComponent table={table} columns={columns} />
+          <TableBodyComponent
+            table={table}
+            columns={columns}
+            isLoading={isLoading}
+            error={error}
+          />
         </Table>
         <Pagination table={table} />
       </div>
