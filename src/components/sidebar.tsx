ec0939af2cdb210287a410db3516fb324109a8bf
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-29 20:16:19 +0100

    feat(ui): Add backdrop to sidebar when open

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -9,35 +9,40 @@ const Sidebar = () => {
   const { isSidebarOpen, sidebarRef } = useSidebarLogic();
 
   return (
-    <div
-      ref={sidebarRef}
-      className={`${
-        isSidebarOpen ? "max-lg:-translate-x-[0%]" : "max-lg:-translate-x-full"
-      } max-lg:fixed z-10 bg-background flex flex-col items-center justify-between w-[240px] h-full py-4 px-3.5 border-r border-tokena-gray dark:border-border transition-transform duration-300 overflow-y-auto overflow-x-hidden`}
-    >
-      <div className="space-y-9">
-        <Image
-          src={logo}
-          alt="logo"
-          priority
-          width={212}
-          height={60}
-          style={{
-            width: "auto",
-            height: "auto",
-          }}
-        />
-        <div className="space-y-5">
-          <span className="text-sm font-medium text-tokena-dark-gray block">
-            Menu
-          </span>
-          <nav>
-            <SidebarLinks />
-          </nav>
+    <>
+      <div
+        ref={sidebarRef}
+        className={`${
+          isSidebarOpen
+            ? "max-lg:-translate-x-[0%]"
+            : "max-lg:-translate-x-full"
+        } max-lg:fixed z-[11] bg-background flex flex-col items-center justify-between w-[240px] h-full py-4 px-3.5 border-r border-tokena-gray dark:border-border transition-transform duration-300 overflow-y-auto overflow-x-hidden`}
+      >
+        <div className="space-y-9">
+          <Image
+            src={logo}
+            alt="logo"
+            priority
+            width={212}
+            height={60}
+            style={{
+              width: "auto",
+              height: "auto",
+            }}
+          />
+          <div className="space-y-5">
+            <span className="text-sm font-medium text-tokena-dark-gray block">
+              Menu
+            </span>
+            <nav>
+              <SidebarLinks />
+            </nav>
+          </div>
         </div>
+        <UserProfile />
       </div>
-      <UserProfile />
-    </div>
+      {isSidebarOpen && <div className="fixed inset-0 bg-black/25 z-10"></div>}
+    </>
   );
 };
 
