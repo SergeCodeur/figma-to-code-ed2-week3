2f60dc98691183d215708832f62a2c289e67282a
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-29 11:11:13 +0100

    feat(utils): Implement useClickOutside hook for handling outside clicks

---
+++ src/hooks/use-outside-click.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+
+const useClickOutside = (callback: () => void, isActive: boolean) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    if (isActive) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [callback, isActive]);
+
+  return ref;
+};
+
+export default useClickOutside;
