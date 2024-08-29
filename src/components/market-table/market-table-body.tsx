e255f326a9fcf742a003ac8650dafddd39174146
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-29 13:29:45 +0100

    feat(table): Implement loading and error states for TableBody

--- src/components/market-table/market-table-body.tsx
+++ src/components/market-table/market-table-body.tsx
@@ -1,15 +1,51 @@
-import { flexRender, Table } from "@tanstack/react-table";
-
+import { Skeleton } from "@/components/ui/skeleton";
 import { TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { flexRender, Table } from "@tanstack/react-table";
 
 interface TableBodyProps<TData> {
   table: Table<TData>;
   columns: any[];
+  isLoading: boolean;
+  error: string | null;
 }
+
 const TableBodyComponent = <TData,>({
   table,
   columns,
+  isLoading,
+  error,
 }: TableBodyProps<TData>) => {
+  if (isLoading) {
+    return (
+      <TableBody>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <TableRow
+            key={index}
+            className="border-tokena-gray dark:border-tokena-gray/25"
+          >
+            {columns.map((_, colIndex) => (
+              <TableCell key={colIndex} className="text-tokena-dark">
+                <Skeleton className="h-4 w-full" />
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    );
+  }
+
+  if (error) {
+    return (
+      <TableBody>
+        <TableRow>
+          <TableCell colSpan={columns.length} className="h-24 text-center">
+            {error}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    );
+  }
+
   return (
     <TableBody>
       {table.getRowModel().rows?.length ? (
