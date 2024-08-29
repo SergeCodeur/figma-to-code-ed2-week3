4cdab4a00fee81dbb401742b5415a6f521c10338
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-29 11:09:36 +0100

    feat(table): Implement custom table hook using Tanstack React Table

---
+++ src/hooks/use-market-table-state.ts
@@ -0,0 +1,58 @@
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useState } from "react";
+
+export function useTableState<TData, TValue>({
+  columns,
+  data,
+}: {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}) {
+  const [rowSelection, setRowSelection] = useState({});
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [sorting, setSorting] = useState<SortingState>([]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+      columnVisibility,
+      rowSelection,
+      columnFilters,
+    },
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageIndex: 0,
+        pageSize: 10,
+      },
+    },
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+
+  return {
+    table,
+    setColumnFilters,
+    setColumnVisibility,
+    setSorting,
+  };
+}
