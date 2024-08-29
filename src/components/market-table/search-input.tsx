534e6e29b0065504ca6e43a399b6606fb96c5e3e
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-29 11:00:09 +0100

    feat(ui): Implement SearchInput component

---
+++ src/components/market-table/search-input.tsx
@@ -0,0 +1,19 @@
+import { Search } from "@/assets/icons";
+import { SearchInputProps } from "@/types";
+import { Input } from "../ui/input";
+
+const SearchInput = ({ value, onChange }: SearchInputProps) => {
+  return (
+    <div className="relative">
+      <Search className="absolute top-1/2 left-2 -translate-y-1/2 text-tokena-dark-gray" />
+      <Input
+        placeholder="Search crypto.."
+        value={value}
+        onChange={onChange}
+        className="h-10 w-full lg:w-[312px] pl-7 text-tokena-dark-gray rounded-xl"
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
