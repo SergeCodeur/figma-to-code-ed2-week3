4ece4e9dc8c848c3f7d5cf58ace46a64bdff3749
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2024-08-29 01:01:18 +0100

    feat(ui): add button component with various elements from shadcnui

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,54 @@
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center gap-1.5 justify-center !cursor-pointer whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-tokena-blue text-tokena-white",
+        outline: "border border-tokena-blue bg-transparent text-tokena-blue",
+        ghost:
+          "text-tokena-blue bg-tokena-blue/[0.06] border border-transparent",
+        "icon-button":
+          "text-tokena-dark-gray dark:border-tokena-dark-blue-2 bg-transparent border-tokena-gray border",
+      },
+      size: {
+        default: "px-5 py-2.5 rounded-xl h-10",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10 rounded-xl",
+        "icon-xs": "h-9 w-9 rounded-xl",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
